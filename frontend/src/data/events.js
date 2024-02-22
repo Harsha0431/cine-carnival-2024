6a3172aa8320f2e5de3b7517aaa1f977499b2ebb
harsha6174 <2200033007@kluniversity.in>
2024-02-23 00:09:22 +0530

    contest guidelines added

--- frontend/src/data/events.js
+++ frontend/src/data/events.js
@@ -3,18 +3,23 @@ export const EventsData = [
     index: 0,
     title: 'Short Film Contest',
     poster: '',
-    image:
-      'https://i.postimg.cc/D2yRNrVQ/3.png',
-    description: 'Welcome to "Imaginary Realms" – a short film contest celebrating the limitless power of creativity! Dive into a world where imagination knows no bounds and storytelling takes center stage. Submit your 5 to 15-minute masterpiece, exploring any genre and language. Surprise us, inspire us, and let your creativity shine! Cash prizes, trophies, and recognition await the most captivating entries. ',
+    image: 'https://i.postimg.cc/D2yRNrVQ/3.png',
+    description:
+      'Welcome to "Imaginary Realms" – a short film contest celebrating the limitless power of creativity! Dive into a world where imagination knows no bounds and storytelling takes center stage. Submit your 5 to 15-minute masterpiece, exploring any genre and language. Surprise us, inspire us, and let your creativity shine! Cash prizes, trophies, and recognition await the most captivating entries. ',
     category: 'LCH - Liberal, Creative and Hobby ',
     type: 'InContest',
     duration: '2 hours',
     club_name: 'Photography Club',
     guidelines: [
-      'Event guideline 1',
-      'Event guideline 2',
-      'Event guideline 3',
-      'Event guideline 4'
+      'Every Team should ensure that every one of their teammates is registered for the event.',
+      'The Duration of the film should not be more than 20 minutes.',
+      'The submissions are screened by the judges in front of everyone keeping the process transparent and clean',
+      'Make sure you are following KL Cine Carnival 2024 Instagram page for all the updates of the venues and event details.',
+      'Everyone is expected to maintain the code of conduct and not misbehave with the organization committee.',
+      'All entries must be original works created by the participating team. Plagiarism or unauthorized use of copyrighted material will result in disqualification.',
+      'Ensure that your film complies with all legal and ethical standards. Avoid any content that promotes violence, hate speech, or discrimination based on race, gender, religion, or nationality.',
+      'The participating team should bring their Film in a hard drive, pen drive or any cloud drive, so that we can seamlessly access your films.',
+      'The best of the works will be recognized and will be awarded rightly.'
     ],
     team_size: 100,
     contact: '21000',
@@ -25,8 +30,7 @@ export const EventsData = [
     index: 1,
     title: 'Reel Making Contest',
     poster: '',
-    image:
-      'https://i.postimg.cc/NQNd5ZVT/4.png',
+    image: 'https://i.postimg.cc/NQNd5ZVT/4.png',
     description: 'Description for Event 2',
     category: 'LCH - Liberal, Creative and Hobby ',
     duration: '2 hours',
@@ -48,17 +52,21 @@ export const EventsData = [
     title: 'AD Film Making Contest',
     poster: '',
     duration: '2 hours',
-    image:
-      'https://i.postimg.cc/R9RKSK2J/5.png',
+    image: 'https://i.postimg.cc/R9RKSK2J/5.png',
     description: 'Description for Event 3',
     category: 'LCH - Liberal, Creative and Hobby ',
     type: 'Contest',
     club_name: 'Photography & Film Technology',
     guidelines: [
-      'Event guideline 1',
-      'Event guideline 2',
-      'Event guideline 3',
-      'Event guideline 4'
+      'Participation is open to individuals and groups, without any age restrictions.',
+      "Advertisements may explore any theme of the participants' choice.",
+      'The duration of the ad film must not exceed the stipulated time limit i.e. 2 minutes.',
+      'Entries must be original creations, avoiding any form of plagiarism or unauthorized use of copyrighted material.',
+      "Advertisements must comply with all legal and ethical standards, refraining from content that incites violence, promotes hate speech, or discriminates against individuals or groups based on race, gender, religion, or nationality.",
+      "Entries will be evaluated based on creativity, effectiveness in conveying the intended message, and overall production quality.",
+      "Judges may consider factors such as originality, storytelling, cinematography, and impact on the audience.",
+      "Ad films will be screened during the contest event, providing an opportunity for contestants to showcase their work to a live audience.",
+      "All participants are expected to conduct themselves professionally and respectfully throughout the contest, interacting courteously with organizers, judges, and fellow contestants."
     ],
     team_size: 100,
     contact: 'Contact details for Event 3',
@@ -69,22 +77,23 @@ export const EventsData = [
     index: 3,
     title: 'Film Poster Making Contest',
     poster: '',
-    image:
-      'https://i.postimg.cc/jxDmxRSL/6.png',
+    image: 'https://i.postimg.cc/jxDmxRSL/6.png',
     description: 'Description for Event 4',
     category: 'LCH - Liberal, Creative and Hobby ',
     duration: '2 hours',
     type: 'Contest',
     club_name: 'Photography & Film Technology',
     guidelines: [
-      'Event guideline 1',
-      'Event guideline 2',
-      'Event guideline 3',
-      'Event guideline 4'
+      "Participants have the creative freedom to design a poster for any film of their choice, without any specific theme restrictions.",
+      "Posters should effectively capture the essence of the chosen film, conveying its mood, genre, and key elements through visual imagery and design elements.",
+      "All submissions must be original creations, avoiding any form of plagiarism or unauthorized use of copyrighted material.",
+      "Entries will be evaluated based on the creativity of the design, its relevance to the chosen film, and the effectiveness of conveying the film's message or theme. Judges may consider factors such as visual impact, composition, typography, and overall artistic expression.",
+      "All participants are expected to conduct themselves professionally and respectfully throughout the contest, interacting courteously with organizers, judges, and fellow contestants.",
+      "Contestants should stay updated on contest-related announcements, guidelines, and deadlines through official communication channels provided by the organizers."
     ],
     team_size: 100,
     contact: 'Contact details for Event 4',
     date: '23rd & 24th February "2K24.',
     register: 'https://klsurabhi.live/event_registration'
-  },
+  }
 ]
